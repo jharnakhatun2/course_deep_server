e587adcf34fdcce03e4c4a756de14c5c564a64c9
jharnakhatun2 <jharnakhatun203@gmail.com>
2025-11-22 09:56:23 +0600

    Solve router and collection ussues

--- app/routes/instructorcourse.js
+++ app/routes/instructorcourse.js
@@ -3,64 +3,64 @@ const router = express.Router();
 const { ObjectId } = require("mongodb");
 const connectToDatabase = require("../db");
 
-// GET all Courses
+// GET all Instructor Courses
 router.get("/", async (req, res) => {
   try {
-    const { coursesDatabase } = await connectToDatabase();
-    const result = await coursesDatabase.find().sort({ _id: -1 }).toArray();
+    const { instructorCoursesDatabase } = await connectToDatabase();
+    const result = await instructorCoursesDatabase.find().sort({ _id: -1 }).toArray();
     res.send(result);
   } catch (err) {
     res.status(500).send("Server error");
   }
 });
 
-// GET course by ID
+// GET Instructor course by ID
 router.get("/:id", async (req, res) => {
   try {
-    const { coursesDatabase } = await connectToDatabase();
+    const { instructorCoursesDatabase } = await connectToDatabase();
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
-    const result = await coursesDatabase.findOne(query);
+    const result = await instructorCoursesDatabase.findOne(query);
     result ? res.send(result) : res.status(404).send("Course not found");
   } catch (err) {
     res.status(500).send("Server error");
   }
 });
 
-// POST new course
+// POST new Instructor course
 router.post("/", async (req, res) => {
   try {
-    const { coursesDatabase } = await connectToDatabase();
+    const { instructorCoursesDatabase } = await connectToDatabase();
     const courseInfo = req.body;
-    const result = await coursesDatabase.insertOne(courseInfo);
+    const result = await instructorCoursesDatabase.insertOne(courseInfo);
     res.send(result);
   } catch (err) {
     res.status(500).send("Server error");
   }
 });
 
-// UPDATE course by ID
+// UPDATE Instructor course by ID
 router.put("/:id", async (req, res) => {
   try {
-    const { coursesDatabase } = await connectToDatabase();
+    const { instructorCoursesDatabase } = await connectToDatabase();
     const id = req.params.id;
     const filter = { _id: new ObjectId(id) };
     const options = { upsert: true };
     const updateDoc = { $set: req.body };
-    const result = await coursesDatabase.updateOne(filter, updateDoc, options);
+    const result = await instructorCoursesDatabase.updateOne(filter, updateDoc, options);
     res.send(result);
   } catch (err) {
     res.status(500).send("Server error");
   }
 });
 
-// DELETE course by ID
+// DELETE Instructor course by ID
 router.delete("/:id", async (req, res) => {
   try {
-    const { coursesDatabase } = await connectToDatabase();
+    const { instructorCoursesDatabase } = await connectToDatabase();
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
-    const result = await coursesDatabase.deleteOne(query);
+    const result = await instructorCoursesDatabase.deleteOne(query);
     res.send(result);
   } catch (err) {
     res.status(500).send("Server error");
